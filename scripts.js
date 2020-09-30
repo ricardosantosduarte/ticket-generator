130f4823b3ac5cf70e59b055364a4ee5ec5c53bb
Ricardo Duarte <ricardosantosduarte1999@gmail.com>
2020-09-30 15:20:09 +0100

    fix: replace last char if traco lol

--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@
 function transform () {
   const ticket = document.getElementById('ticket').value;
   const title = document.getElementById('title').value;
-  var select = document.getElementById("ticket-type");
-  var ticketType = select.options[select.selectedIndex].value;
-  const branch = ticketType + "/" + ticket.toString().toUpperCase() + "-" + title.toString().trim().toLowerCase().replace(/[\W_]+/g, '-');
+  const select = document.getElementById("ticket-type");
+  const ticketType = select.options[select.selectedIndex].value;
+  let branch = ticketType + "/" + ticket.toString().toUpperCase() + "-" + title.toString().trim().toLowerCase().replace(/[\W_]+/g, '-');
+  if (branch.endsWith('-')){
+    branch = branch.slice(0, -1);
+  }
   document.getElementById('branch').innerText = branch;
   const gitCommands = document.getElementById('gitCommands');
   gitCommands.innerText = "git fetch && git checkout "+ branch;
